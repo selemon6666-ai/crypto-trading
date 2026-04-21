542123cc097f7937207386261556cbb79b91d13b
selemon6666-ai <selemon6666@gmail.com>
2026-04-21 20:24:26 +0300

    Update Firebase configuration values

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // firebase-config.js
 const firebaseConfig = {
-    apiKey: "AIzaSyB7LAc7M-K7YByQvjd8-Jx6Q_ddJE1dfC8",
-    authDomain: "Luno-trading.firebaseapp.com",
+    apiKey: "AIzaSyB7lAc7M-K7YByQvjd8-Jx6Q_ddJE1dFC8",
+    authDomain: "luno-trading.firebaseapp.com",
     projectId: "luno-trading",
     storageBucket: "luno-trading.firebasestorage.app",
     messagingSenderId: "124120101873",
-    appId: "1:124120101873:web:364b539fd0c1fb0d83ca50",
+    appId: "1:124120101873:web:3645b39fd0c1fb0d83ca50",
     measurementId: "G-9K36K5MSHW"
 };
 
